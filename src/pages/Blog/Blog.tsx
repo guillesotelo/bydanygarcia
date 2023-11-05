11175cac35fb791ea3f070ad46276ab6c165ac6f
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-11-05 20:25:55 +0100

    ui

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -73,6 +73,12 @@ export default function Blog({ setPost }: Props) {
             <div className="page__header">
                 <h4 className="page__header-title">{category ? category.replace(/_/g, ' ').toUpperCase() : 'OPEN JOURNAL'}</h4>
                 {category ? <h4 className="page__header-subtitle">{catMap[category]}</h4> : ''}
+                <p className="blog__caption">
+                    {lang === 'es' ?
+                        'Me gusta plasmar palabras a mis pensamientos y compartirlos. Me gusta llevar un registro de historias y fotografías. Esto me da un sentido de propósito y satisfacción personal. Pero también es una forma de conectar, acompañar y compartir palabras que pueden sanar, empoderar, inspirar o simplemente sacar una sonrisa.'
+                        :
+                        'I like to put word to my thoughts and share them.  I like to keep record of stories and photographs. It gives me a sense of purpose and personal fulfillment. But also, it is a way to connect, accompany and share words can that can heal, empower, inspire or just bring out a smile.'}
+                </p>
             </div>
             {loading ? <span className="loader"></span>
                 :
@@ -80,12 +86,6 @@ export default function Blog({ setPost }: Props) {
                     {allPosts.map((post, i) => <PostCard setPost={setPost} post={post} />)}
                 </div>
             }
-            <p className="blog__caption">
-                {lang === 'es' ?
-                    'Me gusta plasmar palabras a mis pensamientos y compartirlos. Me gusta llevar un registro de historias y fotografías. Esto me da un sentido de propósito y satisfacción personal. Pero también es una forma de conectar, acompañar y compartir palabras que pueden sanar, empoderar, inspirar o simplemente sacar una sonrisa.'
-                    :
-                    'I like to put word to my thoughts and share them.  I like to keep record of stories and photographs. It gives me a sense of purpose and personal fulfillment. But also, it is a way to connect, accompany and share words can that can heal, empower, inspire or just bring out a smile.'}
-            </p>
         </div>
     )
 }
