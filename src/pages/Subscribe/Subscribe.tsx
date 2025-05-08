cb0c8ec1c35706e973d98bb75aa085aa282b5efc
Guillermo <guille.sotelo.cloud@gmail.com>
2025-05-08 15:38:02 +0200

    store-finished-stage1

--- src/pages/Subscribe/Subscribe.tsx
+++ src/pages/Subscribe/Subscribe.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import { useContext, useState } from 'react'
 import InputField from '../../components/InputField/InputField'
 import Button from '../../components/Button/Button'
 import { useHistory } from 'react-router-dom'
@@ -7,7 +7,6 @@ import { subscribe } from '../../services/app'
 import { TEXT } from '../../constants/lang'
 import { AppContext } from '../../AppContext'
 import { onChangeEventType } from '../../types'
-import { history } from '../../helpers'
 
 type Props = {}
 
@@ -22,43 +21,45 @@ export default function Subscribe({ }: Props) {
     }
 
     const onSubscribe = async () => {
-        const loading = toast.loading(TEXT[lang]['subscribing'])
-        if (!data.fullname.includes(' ') || !data.email.includes('@') || !data.email.includes('.')) {
-            toast.error(lang === 'es' ? 'Checkea los campos' : 'Check the fields')
-            return toast.remove(loading)
-        }
-        const logged = await subscribe(data)
-        if (logged) {
-            toast.success(TEXT[lang]['subscribe_ok'])
-            setTimeout(() => history.push('/'), 1500)
-        } else toast.error(TEXT[lang]['subscribe_error'])
+        try {
+            const loading = toast.loading(TEXT[lang]['subscribing'])
+            if (!data.fullname.includes(' ') || !data.email.includes('@') || !data.email.includes('.')) {
+                toast.error(lang === 'es' ? 'Checkea los campos' : 'Check the fields')
+                return toast.remove(loading)
+            }
+            const logged = await subscribe(data)
+            if (logged) {
+                toast.success(TEXT[lang]['subscribe_ok'])
+                setTimeout(() => history.push('/'), 1500)
+            } else toast.error(TEXT[lang]['subscribe_error'])
 
-        return toast.remove(loading)
+            return toast.remove(loading)
+        } catch (error) {
+            toast.error(TEXT[lang]['subscribe_error'])
+        }
     }
 
     return (
         <div className='subscribe__container'>
-            <div className="subscribe-col">
-                <h2>{lang === 'es' ? 'Únete a mi Comunidad' : 'Join my Mail Community'}</h2>
-                <h3>{lang === 'es' ? 'Únete y recibe cartas mensuales 🖤' : 'Sign up for monthly letters 🖤'}</h3>
-            </div>
-            <div className="subscribe__box">
+            <h2 style={{ fontFamily: '"Madelyn", sans-serif', fontSize: '5rem', margin: '2rem 0 0 0' }}>{lang === 'es' ? 'Únete a mi Comunidad' : 'Join my Mail Community'}</h2>
+            <h3 style={{ fontSize: '1.3rem', fontWeight: 'normal' }}>{lang === 'es' ? 'Únete y recibe cartas mensuales 🖤' : 'Sign up for monthly letters 🖤'}</h3>
+            <div className="postviewer__subscribe-row">
                 <InputField
                     name='fullname'
                     updateData={updateData}
-                    placeholder={TEXT[lang]['full_name']}
+                    placeholder='Your full name'
                 />
                 <InputField
                     name='email'
                     updateData={updateData}
-                    placeholder={TEXT[lang]['your_email']}
+                    placeholder='Your email'
                     type='email'
                 />
                 <Button
                     label={lang === 'es' ? 'Únete' : 'Join'}
                     handleClick={onSubscribe}
                     disabled={!data.email || !data.fullname}
-                    style={{ width: '100%' }}
+                    style={{ width: isMobile ? '100%' : '' }}
                 />
             </div>
         </div >
